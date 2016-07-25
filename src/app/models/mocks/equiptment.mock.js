369b4921a59a43f574fcdfb26a8aa5aee427b0ef
Lisa Kegler <lkegler@sandia.gov>
2016-07-25 08:36:52 -0600

    adding missing files

---
+++ src/app/models/mocks/equiptment.mock.js
@@ -0,0 +1,70 @@
+'use strict';
+/**
+ * @ngdoc function
+ * @name insideApp.module:supportTeams
+ * @description
+ * # supportTeams
+ * mockData for supportTeams of the insideApp
+ */
+angular.module('vactApp.mocks')
+    .constant('EQUIPMENT_MOCK',
+        {
+          "source": [
+            {
+              "type": "laptop",
+              "name": "laptop1"
+            },
+            {
+              "type": "laptop",
+              "name": "laptop2"
+            },
+            {
+              "type": "laptop",
+              "name": "laptop3"
+            },
+            {
+              "type": "laptop",
+              "name": "laptop4"
+            },
+            {
+              "type": "desktop",
+              "name": "desktop1"
+            },
+            {
+              "type": "document camera",
+              "name": "documentCamer1"
+            },
+            {
+              "type": "document camera",
+              "name": "documentCamer2"
+            },
+            {
+              "type": "video camera",
+              "name": "videoCamera1"
+            },
+            {
+              "type": "video camera",
+              "name": "video camera 2"
+            }
+
+          ],
+          "target": [
+            {
+              "type": "lcd monitor",
+              "name": "lcd monitor 1"
+            },
+            {
+              "type": "lcd monitor",
+              "name": "lcd monitor 2"
+            },
+            {
+              "type": "lcd monitor",
+              "name": "lcd monitor 3"
+            },
+            {
+              "type": "projector",
+              "name": "projector 1"
+            }
+          ]
+        }
+    );
---
+++ src/app/models/mocks/iguanaServer.mock.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = function (request) {
+    console.log('Loaded Iguana Server mock');
+    var connection = request.accept(null, request.origin);
+
+    console.log('client connected: ', request.origin);
+
+    connection.on('message', function (message) {
+        if (message.type === 'utf8') {
+            var object = JSON.parse(message.utf8Data);
+
+            console.log('received:', object);
+
+            setTimeout(function () {
+                connection.sendUTF(JSON.stringify(object));
+            }, 2000);
+        }
+    });
+
+    connection.on('close', function (connection) {
+        console.log('connection closed');
+    });
+};
