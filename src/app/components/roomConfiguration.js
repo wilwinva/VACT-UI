e4de1a96afe2190548d563eb3001063f0008fb87
Lisa Kegler <lkegler@sandia.gov>
2016-07-26 18:24:34 -0600

    synced targets and sources

--- src/app/components/roomConfiguration.js
+++ src/app/components/roomConfiguration.js
@@ -18,7 +18,27 @@ angular.module('vactApp')
         self.targets = self.room_properties.target;
         self.configuration = self.room_properties.configuration;
 
-        var emptySet = {"type": "none", "label": "None", "id": "none", "source": ""};
+        for (var sourceIndex = 0; sourceIndex < self.sources.length; sourceIndex++) {
+            var sourceId = self.sources[sourceIndex].id;
+            for (var configSourceIndex = 0; configSourceIndex < self.configuration.length; configSourceIndex++) {
+                if (self.configuration[configSourceIndex].source === sourceId) {
+                    self.sources[sourceIndex].target = self.configuration[configSourceIndex].target;
+                    break;
+                }
+            }
+        }
+
+        for (var targetIndex = 0; targetIndex < self.targets.length; targetIndex++) {
+            var targetId = self.targets[targetIndex].id;
+            for (var configTargetIndex = 0; configTargetIndex < self.configuration.length; configTargetIndex++) {
+                if (self.configuration[configTargetIndex].target === targetId) {
+                    self.targets[targetIndex].source = self.configuration[configTargetIndex].source;
+                    break;
+                }
+            }
+        }
+
+        var emptySet = {'type': 'none', 'label': 'None', 'id': 'none', 'source': ''};
         self.sources.push(emptySet);
         self.targets.push(emptySet);
 
@@ -26,13 +46,19 @@ angular.module('vactApp')
             var sourceId = source.id;
             //need to test for targets displaying something already
             var inUse = false;
-            for (var i = 0; i < self.configuration.length; i++) {
-                if (self.configuration[i].target === targetId) {
-                    inUse = true;
-                    if (window.confirm(targetId + " is already in use. Would you like to display this instead?")) {
-                        self.configuration.splice(i);
-                        //TODO: do we need to send a message when we move something off of the display
-                        inUse = false;
+
+            for (var tIndex = 0; tIndex < self.targets.length; tIndex++) {
+                if (self.targets[tIndex].id === targetId) {
+                    if (self.targets[tIndex].source && self.targets[tIndex].source !== 'none') {
+                        inUse = true;
+                        if (window.confirm(targetId + ' is already in use. Would you like to display this instead?')) {
+                            //TODO: do we need to send a message when we move something off of the display
+                            inUse = false;
+                            self.targets[tIndex].source = sourceId;
+                        }
+                    }
+                    else {
+                        self.targets[tIndex].source = sourceId;
                     }
                     break;
                 }
@@ -48,6 +74,14 @@ angular.module('vactApp')
         self.sourceSelected = function (target, sourceId) {
             //sources can be displayed on multiple targets
             var targetId = target.id;
+
+            for (var sIndex = 0; sIndex < self.sources.length; sIndex++) {
+                if (self.sources[sIndex].id === sourceId) {
+                    self.sources[sIndex].target = targetId;
+                    break;
+                }
+            }
+
             var sendObj = self.buildSendObj(sourceId, targetId);
             self.configuration.push(sendObj);
             console.log('sendObj: ' + sendObj);
@@ -61,7 +95,8 @@ angular.module('vactApp')
         };
 
         self.filterNone = function (menuItem) {
-            return menuItem.id === "none" ? false : true;
+            return menuItem.id === 'none' ? false : true;
         };
-    }])
+    }
+    ])
 ;
