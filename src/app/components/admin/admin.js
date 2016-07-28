935c7ddc1883024fa235e3be91103da154fd3539
William Weatherby <wbweath@sandia.gov>
2016-07-28 10:56:11 -0600

    adding admin logic

--- src/app/components/admin/admin.js
+++ src/app/components/admin/admin.js
@@ -36,26 +36,74 @@ angular.module('vactApp')
       self.showEquipmentSpecifics = false;
 
       self.equipmentSpecifics = [];
+      self.destinationArray = [];
 
       self.equipmentType = '';
       self.equipmentLabel = '';
       self.equipmentId = '';
-      self.equipmentSecured = '';
+      self.equipmentSecured = false;
 
-      self.equipmentSelected = function(equipment){
+      self.equipmentSelected = function(equipment, equipmentGroup){
         console.log('made it in equipmentSelected:'+equipment);
         //need to find the equipment object in the existing arrays
+        self.findEquipmentObjByReference(equipment, equipmentGroup);
 
-        self.equipmentType = '';
-        self.equipmentLabel = '';
-        self.equipmentId = '';
-        self.equipmentSecured = '';
         self.showEquipmentSpecifics = true;
       };
 
-      self.findEquipmentObjByReference = function(){
+      self.findEquipmentObjByReference = function(equipmentType, equipmentGroup){
+        var arrayToSearch = [];
+        switch(equipmentGroup){
+          case 'computers':
+            arrayToSearch = self.computers;
+            break;
+          case 'displays':
+            arrayToSearch = self.displays;
+            break;
+          case 'cameras':
+            arrayToSearch = self.cameras;
+            break;
+          case 'peripheral':
+            arrayToSearch = self.peripheral;
+            break;
+        }
+        var newObj = {};
+        for(var i=0;i<arrayToSearch.length;i++){
+          if(arrayToSearch[i].type === equipmentType){
+            newObj = arrayToSearch[i];
+          }
+        }
+        //check to see if this type of equipment is already ing the source or target lists
+        if(equipmentGroup === 'displays'){
+          arrayToSearch = self.newTargets;
+        }else{
+          arrayToSearch = self.newSources;
+        }
+        self.destinationArray = arrayToSearch;
 
-      }
+        var increment = 1;
+        if(arrayToSearch.length > 0){
+          for(var i=0;i<arrayToSearch.length;i++){
+            if(arrayToSearch[i].type === equipmentType){
+              increment++;
+            }
+          }
+        }
+        self.equipmentType = newObj.type;
+        self.equipmentLabel = newObj.label + ' ' + increment;
+        self.equipmentId = newObj.id + increment;
+      };
+
+      self.saveEquipment = function(){
+        var newEquipment = {};
+        newEquipment.type = self.equipmentType;
+        newEquipment.label = self.equipmentLabel;
+        newEquipment.id = self.equipmentId;
+        newEquipment.secured = self.equipmentSecured;
+        self.destinationArray.push(newEquipment);
+
+      };
+
 
       self.setEquipmentList = function(activeList){
         console.log('made it in setEquipmentList');
